6829e641680b10f746676dbfcf046bdebdea8aa4
hudeshun <dshu@nullmax.ai>
2019-07-17 18:02:15 +0800

    Modify first so3 test case.

--- test/test_so3.cc
+++ test/test_so3.cc
@@ -3,8 +3,8 @@
 
 TEST_CASE( "Test so3 initialization", "[node]") {
     hitnlls::node::SO3 so3;
-    REQUIRE(so3.W() == 1);
-    REQUIRE(so3.X() == 0);
-    REQUIRE(so3.Y() == 0);
-    REQUIRE(so3.Z() == 0);
+    REQUIRE(so3.W() == Approx(1));
+    REQUIRE(so3.X() == Approx(0));
+    REQUIRE(so3.Y() == Approx(0));
+    REQUIRE(so3.Z() == Approx(0));
 }
